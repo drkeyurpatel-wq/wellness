f48a4d7575374bbd7528e2a1fe039702fb9f1fd6
drkeyurpatel-wq <drkeyurpatel@gmail.com>
2026-06-23 11:47:27 +0000

    Add gym tracker PWA at /gym (PPL plan, weight goal-line chart, streaks)

---
+++ gym/sw.js
@@ -0,0 +1,20 @@
+var CACHE="h1train-v1";
+var ASSETS=["./","./index.html","./manifest.json"];
+self.addEventListener("install",function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS)}));
+  self.skipWaiting();
+});
+self.addEventListener("activate",function(e){
+  e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(k){return k!==CACHE}).map(function(k){return caches.delete(k)}))}));
+  self.clients.claim();
+});
+self.addEventListener("fetch",function(e){
+  if(e.request.method!=="GET")return;
+  e.respondWith(
+    caches.match(e.request).then(function(r){
+      return r||fetch(e.request).then(function(resp){
+        return caches.open(CACHE).then(function(c){try{c.put(e.request,resp.clone())}catch(x){} return resp});
+      }).catch(function(){return caches.match("./index.html")});
+    })
+  );
+});
